a1844db15cdb730da0a5d53415c46fd37c8f35c9
Nate Faber <nathaniel@njfaber.com>
2021-05-01 19:40:35 +0200

    Lint code

--- test/Unit.js
+++ test/Unit.js
@@ -116,31 +116,41 @@ contract("C2", async (acc) => {
     const amountToIssue = 300000;
 
     // Once tokens are issued, should not considered funded
-    await c2.issue(acc[1], amountToIssue)
-    assert.isFalse(await c2.isFunded.call())
+    await c2.issue(acc[1], amountToIssue);
+    assert.isFalse(await c2.isFunded.call());
 
-    const amountNeededToFund = (await c2.totalBackingNeededToFund.call()).toNumber();
-    const remainingToFund = (await c2.remainingBackingNeededToFund.call()).toNumber();
+    const amountNeededToFund = (
+      await c2.totalBackingNeededToFund.call()
+    ).toNumber();
+    const remainingToFund = (
+      await c2.remainingBackingNeededToFund.call()
+    ).toNumber();
     assert.equal(amountNeededToFund, remainingToFund);
-    assert.isAbove(amountNeededToFund, 0)
+    assert.isAbove(amountNeededToFund, 0);
 
     const firstFunding = Math.floor(amountNeededToFund / 10);
-    await bac.approve(c2.address, firstFunding)
+    await bac.approve(c2.address, firstFunding);
     await c2.fund(firstFunding);
 
-    const amountNeededToFund2 = (await c2.totalBackingNeededToFund.call()).toNumber();
-    const remainingToFund2 = (await c2.remainingBackingNeededToFund.call()).toNumber();
-    assert.equal(amountNeededToFund, amountNeededToFund2)
-    assert.equal(remainingToFund2, amountNeededToFund - firstFunding)
+    const amountNeededToFund2 = (
+      await c2.totalBackingNeededToFund.call()
+    ).toNumber();
+    const remainingToFund2 = (
+      await c2.remainingBackingNeededToFund.call()
+    ).toNumber();
+    assert.equal(amountNeededToFund, amountNeededToFund2);
+    assert.equal(remainingToFund2, amountNeededToFund - firstFunding);
     assert.isFalse(await c2.isFunded.call());
 
     // Fund remaining
-    await bac.approve(c2.address, remainingToFund2)
-    await c2.fund(remainingToFund2)
+    await bac.approve(c2.address, remainingToFund2);
+    await c2.fund(remainingToFund2);
 
     assert.isTrue(await c2.isFunded.call());
-    const remainingToFund3 = (await c2.remainingBackingNeededToFund.call()).toNumber();
-    assert.equal(remainingToFund3, 0)
+    const remainingToFund3 = (
+      await c2.remainingBackingNeededToFund.call()
+    ).toNumber();
+    assert.equal(remainingToFund3, 0);
   });
 
   it.skip("refunds overfunding to owner", async () => {
